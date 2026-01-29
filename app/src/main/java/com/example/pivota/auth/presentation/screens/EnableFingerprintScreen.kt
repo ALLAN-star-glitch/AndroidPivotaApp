929f43fca0e620db817671cf2df24e2ce632f046
martin <martinmichuki8@gmail.com>
2026-01-29 10:19:50 +0300

    added the enable fingerprint screen and associated icons

---
+++ app/src/main/java/com/example/pivota/auth/presentation/screens/EnableFingerprintScreen.kt
@@ -0,0 +1,159 @@
+package com.example.pivota.auth.presentation.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.pivota.R
+
+@Preview(showBackground = true)
+@Composable
+fun EnableFingerprintScreen() {
+    Scaffold(
+        bottomBar = {
+            Column (
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Button(
+                    onClick = { /* Enable */ },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    shape = RoundedCornerShape(50)
+                ) {
+                    Text("Enable Fingerprint", fontWeight = FontWeight.Bold)
+                }
+
+                Text(
+                    "Maybe later",
+                    color = MaterialTheme.colorScheme.primary,
+                    fontWeight = FontWeight.SemiBold,
+                    modifier = Modifier.clickable { /* Skip logic */ }
+                )
+
+                Spacer(Modifier.height(24.dp))
+            }
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Spacer(Modifier.height(24.dp))
+
+            Surface (
+                shape = CircleShape,
+                color = MaterialTheme.colorScheme.primaryContainer,
+                modifier = Modifier.size(56.dp)
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.fingerprint_24px), // replace
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.onPrimaryContainer,
+                    modifier = Modifier.size(48.dp).padding(all = 8.dp)
+                )
+            }
+
+            Spacer(Modifier.height(24.dp))
+
+            Text(
+                text = "Enable fingerprint login",
+                style = MaterialTheme.typography.headlineSmall,
+                fontWeight = FontWeight.SemiBold
+            )
+
+            Spacer(Modifier.height(16.dp))
+
+            Text(
+                text = "Use your fingerprint to sing in faster and keep your account secure. You can change this anytime in settings.",
+                textAlign = TextAlign.Center
+            )
+
+            LazyColumn(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                item {
+                    BenefitCard(
+                        icon = painterResource(id = R.drawable.bolt_24px),
+                        benefit = "Faster sign-in"
+                    )
+                }
+
+                item {
+                    BenefitCard(
+                        icon = painterResource(id = R.drawable.verified_user_24px),
+                        benefit = "Extra security"
+                    )
+                }
+
+                item {
+                    BenefitCard(
+                        icon = painterResource(id = R.drawable.key_24px),
+                        benefit = "No passwords to remember"
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BenefitCard(icon: Painter, benefit: String) {
+    Row(
+        modifier = Modifier.fillMaxWidth()
+            .padding(vertical = 16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box (
+            modifier = Modifier
+                .size(40.dp)
+                .background(
+                    color = MaterialTheme.colorScheme.primaryContainer,
+                    shape = RoundedCornerShape(12.dp)
+                ),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                painter = icon,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.primary,
+            )
+        }
+
+        Spacer(Modifier.width(16.dp))
+
+        Text(text = benefit, fontWeight = FontWeight.SemiBold)
+    }
+}
